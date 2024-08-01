0812bb7446b488a7c95dac8a7b7d6fd2cf474a17
marakhd <marakin09@mail.ru>
2024-08-02 02:52:17 +0300

    Add pathlib for real path .env

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pathlib import Path
 
 class Settings(BaseSettings):
     CMC_API_KEY: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=Path(__file__).resolve().parent / ".env")
 
     
 settings = Settings()
